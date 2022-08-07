15e2a59ef191e19d68251c443aaecc542ea58e0b
selvam saravana <sec20cs143@sairamtap.edu.in>
2022-08-07 10:02:25 +0530

    removed stext

--- lib/widgets/toprated.dart
+++ lib/widgets/toprated.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/utils/stext.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app/widgets/desc.dart';
 
 class Toprated extends StatelessWidget {
@@ -13,22 +13,22 @@ class Toprated extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Stext(
-            text: 'Top Rated Movies',
-            size: 25,
+          Text(
+            'Top Rated Movies',
+            style: GoogleFonts.breeSerif(fontSize: 25),
           ),
           const SizedBox(
             height: 20,
           ),
           SizedBox(
-              height:270,
+              height: 270,
               child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemCount: toprated.length,
                   itemBuilder: (context, index) {
                     return InkWell(
                       onTap: () {
-                         Navigator.of(context).push(
+                        Navigator.of(context).push(
                           MaterialPageRoute(
                             builder: (context) => Description(
                                 name: toprated[index]['title'],
@@ -43,31 +43,32 @@ class Toprated extends StatelessWidget {
                           ),
                         );
                       },
-                      child:toprated[index]['title'] != null?SizedBox(
-                        width: 140,
-                        child: SingleChildScrollView(
-                          child: Column(children: [
-                            Container(
-                              height: 200,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                    'https://image.tmdb.org/t/p/w500' +
-                                        toprated[index]['poster_path'],
+                      child: toprated[index]['title'] != null
+                          ? SizedBox(
+                              width: 140,
+                              child: SingleChildScrollView(
+                                child: Column(children: [
+                                  Container(
+                                    height: 200,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          'https://image.tmdb.org/t/p/w500' +
+                                              toprated[index]['poster_path'],
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text( toprated[index]['title'] != null
+                                        ? toprated[index]['title']
+                                        : 'Loading',style: GoogleFonts.breeSerif(fontSize: 15),)
+                                ]),
                               ),
-                            ),
-                            const SizedBox(height: 8,),
-                            Stext(
-                              text: toprated[index]['title'] != null
-                                  ? toprated[index]['title']
-                                  : 'Loading',
-                              size: 15,
                             )
-                          ]),
-                        ),
-                      ):Container(),
+                          : Container(),
                     );
                   })),
         ],
